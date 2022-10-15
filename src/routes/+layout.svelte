7838cbaba2be9b09011115c89b199070d5211a4f
Jonathan E. Emmett <jon@emmettmedia.com>
2022-10-15 17:18:26 -0500

    there was a weird build error, I had to copy over .eslintignore down from a working project to get it building

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,19 @@
 <script>
-    import Navbar from '../lib/components/layout/Navbar.svelte';
-    import '../styles/global.css';
+	import Navbar from '../lib/components/layout/Navbar.svelte';
+	import '../styles/global.css';
 </script>
 
 <header>
-    <Navbar />  
+	<Navbar />
 </header>
 <main>
-    <slot />
+	<slot />
 </main>
 
 <style>
-    main {
-        max-width: 1290px;
-        margin: 0 auto;
-        padding: 0 1rem;
-    }
+	main {
+		max-width: 1290px;
+		margin: 0 auto;
+		padding: 0 1rem;
+	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$db: path.resolve('./src/db')
+		}
 	}
 };
 
